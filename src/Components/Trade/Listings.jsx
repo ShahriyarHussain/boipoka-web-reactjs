6529b49ed55bb68181133d3ef293bab12b513f53
ShahriyarHussain <shossainshihab36@gmail.com>
2022-01-04 04:11:14 +0600

    Cleanup

--- src/Components/Trade/Listings.jsx
+++ src/Components/Trade/Listings.jsx
@@ -1,5 +1,5 @@
 import CheckLogin from "../../Hooks/CheckLogin";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import React, { useState, useContext, useEffect } from "react";
 import ListingCard from "./ListingCard";
 
