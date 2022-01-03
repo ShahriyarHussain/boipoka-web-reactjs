92e5b1225916d1ad15361ad6db758f79d55f9304
ShahriyarHussain <shossainshihab36@gmail.com>
2022-01-03 17:12:51 +0600

    Add register loading

--- src/Components/UserAuthentication/Register.jsx
+++ src/Components/UserAuthentication/Register.jsx
@@ -14,6 +14,7 @@ function Register() {
   });
   const [pass, setpass] = useState("");
   const [message, setmessage] = useState("Development preview");
+  const [isPending, setIsPending] = useState(false);
 
   let navigate = useNavigate();
 
@@ -48,7 +49,7 @@ function Register() {
       setmessage("Please fill up the fields correctly");
       return false;
     }
-
+    setIsPending(true);
     const { firstName, lastName, username, password } = userData;
     Axios.post(url + "users/users/create", {
       user: {
@@ -59,6 +60,7 @@ function Register() {
       },
     }).then((response) => {
       if (response.status === 200) {
+        setIsPending(false);
         navigate("/login");
       } else {
         setmessage("Error! Code: " + response.status);
@@ -68,106 +70,109 @@ function Register() {
 
   return (
     <div>
-      <div className='flex flex-col items-center'>
+      <div className="flex flex-col items-center">
         <img
-          className='rounded-full p-2 mt-2 bg-darkblue'
+          className="rounded-full p-2 mt-2 bg-darkblue"
           src={logo}
-          height='100px'
-          width='100px'
-          alt='Boipoka Logo'
+          height="100px"
+          width="100px"
+          alt="Boipoka Logo"
         />
-        <h2 className='font-extrabold text-center text-3xl p-2 mt-2 mb-4'>
+        <h2 className="font-extrabold text-center text-3xl p-2 mt-2 mb-4">
           Create A Boipoka Account
         </h2>
       </div>
 
-      <div className='flex flex-col items-center'>
+      <div className="flex flex-col items-center">
         <form
-          className='flex flex-col justify-center p-2 m-2 max-w-md h-auto w-auto'
-          onSubmit={registerHandler}>
+          className="flex flex-col justify-center p-2 m-2 max-w-md h-auto w-auto"
+          onSubmit={registerHandler}
+        >
           <label>First Name</label>
           <input
-            className='rounded-md border-2 border-gray-200 drop-shadow-sm h-8'
+            className="rounded-md border-2 border-gray-200 drop-shadow-sm h-8"
             required
-            type='text'
-            name='firstName'
+            type="text"
+            name="firstName"
             value={userData.firstName}
             onChange={setData}
           />
-          <p className='text-red-600 mt-2 mb-3 text-xs'>
+          <p className="text-red-600 mt-2 mb-3 text-xs">
             {validate(userData.firstName)
               ? ""
               : "Must be within 3-20 characters"}
           </p>
 
-          <label className='m-2'>Last Name</label>
+          <label className="m-2">Last Name</label>
           <input
-            className='rounded-md border-2 border-gray-200 drop-shadow-sm h-8 text-lg '
+            className="rounded-md border-2 border-gray-200 drop-shadow-sm h-8 text-lg "
             required
-            type='text'
-            name='lastName'
+            type="text"
+            name="lastName"
             value={userData.lastName}
             onChange={setData}
           />
-          <p className='text-red-600 mt-2 mb-3 text-xs'>
+          <p className="text-red-600 mt-2 mb-3 text-xs">
             {validate(userData.lastName)
               ? ""
               : "Must be within 3-20 characters"}
           </p>
 
-          <label className='m-2'>Username</label>
+          <label className="m-2">Username</label>
           <input
-            className='rounded-md border-2 border-gray-200 drop-shadow-sm h-8 text-lg'
+            className="rounded-md border-2 border-gray-200 drop-shadow-sm h-8 text-lg"
             required
-            type='text'
-            name='username'
+            type="text"
+            name="username"
             value={userData.username}
             onChange={setData}
           />
-          <p className='text-red-600 mt-2 mb-3 text-xs'>
+          <p className="text-red-600 mt-2 mb-3 text-xs">
             {validate(userData.username)
               ? ""
               : "Must be within 3-20 characters"}
           </p>
 
-          <label className='m-2'>Password</label>
+          <label className="m-2">Password</label>
           <input
-            className='rounded-md border-2 border-gray-200 drop-shadow-sm h-8 text-lg'
+            className="rounded-md border-2 border-gray-200 drop-shadow-sm h-8 text-lg"
             required
-            type='password'
-            name='password'
+            type="password"
+            name="password"
             value={userData.password}
             onChange={setData}
           />
 
-          <label className='m-2'>Confirm Password</label>
+          <label className="m-2">Confirm Password</label>
           <input
-            className='rounded-md border-2 border-gray-200 drop-shadow-sm h-8 text-lg'
+            className="rounded-md border-2 border-gray-200 drop-shadow-sm h-8 text-lg"
             required
-            type='password'
-            name='pass'
+            type="password"
+            name="pass"
             value={pass}
             onChange={(e) => setpass(e.target.value)}
           />
-          <p className='text-red-600 mt-2 mb-3 text-xs'>
+          <p className="text-red-600 mt-2 mb-3 text-xs">
             {userData.password === pass ? "" : "Passwords Don't match"}
           </p>
 
-          <button className='bg-mildorange text-darkblue font-bold rounded-3xl p-2 mt-4 max-h-10 max-w-md'>
+          <button className="bg-mildorange text-darkblue font-bold rounded-3xl p-2 mt-4 max-h-10 max-w-md">
             Register
           </button>
         </form>
 
-        <div className='mt-3 p-3 font-bold text-md'>
-          <p className='text-darkblue'>
+        <div className="mt-3 p-3 font-bold text-md">
+          <p className="text-darkblue">
             Already have an account?
-            <Link to='/login'>
-              <span className='text-yellow-500 ml-2'> Log in</span>
+            <Link to="/login">
+              <span className="text-yellow-500 ml-2"> Log in</span>
             </Link>
           </p>
         </div>
 
-        <p className='text-red-600 mt-3 font-bold text-xl'>{message}</p>
+        <p className="text-red-600 mt-3 font-bold text-xl">
+          {isPending ? "Creating Account" : message}
+        </p>
       </div>
     </div>
   );
